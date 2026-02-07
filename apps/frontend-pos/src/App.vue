dc946c5d6582acae1d11b300e7fbd81f16ba7ada
Nguyễn Nguyệt Ánh <128211112+hikawi@users.noreply.github.com>
2026-02-07 17:07:06 +0700

    chore(pos): tried to add some ui components library

--- apps/frontend-pos/src/App.vue
+++ apps/frontend-pos/src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { DummyComponent } from "@foodbasket/ui";
+</script>
 
 <template>
-  <h1>You did it! You should be on *.pos.foodbasket.app</h1>
+  <h1 class="">You did it! You should be on *.pos.foodbasket.app</h1>
+  <DummyComponent />
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,3 @@
+export { default as DummyComponent } from "./components/DummyComponent.vue";
+
+import "./styles.css";
