a392f39f6749312a90cd40297bdf71f51bbe4683
Nguyễn Nguyệt Ánh <128211112+hikawi@users.noreply.github.com>
2026-02-26 05:39:07 +0700

    chore: setup centralized dockerfile

--- apps/backend-rs/migrations/20260225083327_default_random_uuid.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
-
-ALTER TABLE users ALTER COLUMN id SET DEFAULT gen_random_uuid();
-ALTER TABLE tenants ALTER COLUMN id SET DEFAULT gen_random_uuid();
-ALTER TABLE roles ALTER COLUMN id SET DEFAULT gen_random_uuid();
