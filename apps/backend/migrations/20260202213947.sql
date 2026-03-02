691a7f3d010f5a4ccaa0346e617120b71938ac11
Nguyễn Nguyệt Ánh <128211112+hikawi@users.noreply.github.com>
2026-03-03 01:38:10 +0700

    chore(backend): remove all go files

--- apps/backend/migrations/20260202213947.sql
+++
@@ -1,4 +0,0 @@
--- Modify "tenants" table
-ALTER TABLE "tenants" ADD COLUMN "slug" text NOT NULL;
--- Create index "idx_tenants_slug" to table: "tenants"
-CREATE UNIQUE INDEX "idx_tenants_slug" ON "tenants" ("slug") WHERE (deleted_at IS NULL);
